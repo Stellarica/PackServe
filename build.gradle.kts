03a4863837dd59eeb7426eddfbb09b60186835eb
trainb0y <trainb0ydev@gmail.com>
2023-05-06 17:54:29 -0600

    Basic Velocity plugin

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "1.8.21"
+}
+
+group = "net.stellarica"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    maven {
+        name = "papermc"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+dependencies {
+    compileOnly("com.velocitypowered:velocity-api:3.1.1")
+    annotationProcessor("com.velocitypowered:velocity-api:3.1.1")
+}
+
+kotlin {
+    jvmToolchain(17)
+}
---
+++ src/main/kotlin/net/stellarica/packserve/PackServe.kt
@@ -0,0 +1,30 @@
+package net.stellarica.packserve
+
+import com.google.inject.Inject
+import com.velocitypowered.api.event.Subscribe
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent
+import com.velocitypowered.api.plugin.Plugin
+import com.velocitypowered.api.plugin.annotation.DataDirectory
+import com.velocitypowered.api.proxy.ProxyServer
+import org.slf4j.Logger
+import java.nio.file.Path
+
+@Suppress("Unused")
+@Plugin(
+	id = "packserve",
+	name = "PackServe",
+	version = "0.1.0",
+	url = "https://github.com/Stellarica/PackServe",
+	description = "Resource pack stuff:tm:",
+	authors = ["trainb0y"]
+)
+class PackServe @Inject constructor(
+	val server: ProxyServer,
+	val logger: Logger,
+	@DataDirectory val dataDir: Path
+) {
+	@Subscribe
+	fun onProxyInit(event: ProxyInitializeEvent) {
+
+	}
+}
