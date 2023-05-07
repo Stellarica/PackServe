675f212dc648750ef293cf59a0f399ac57238476
trainb0y <trainb0ydev@gmail.com>
2023-05-06 21:17:49 -0600

    Some random experimentation

--- src/main/kotlin/net/stellarica/packserve/PackServe.kt
+++ src/main/kotlin/net/stellarica/packserve/PackServe.kt
@@ -4,13 +4,17 @@ import com.google.inject.Inject
 import com.sksamuel.hoplite.ConfigLoaderBuilder
 import com.sksamuel.hoplite.addFileSource
 import com.velocitypowered.api.event.Subscribe
+import com.velocitypowered.api.event.player.PlayerChatEvent
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent
 import com.velocitypowered.api.plugin.Plugin
 import com.velocitypowered.api.plugin.annotation.DataDirectory
 import com.velocitypowered.api.proxy.ProxyServer
+import net.kyori.adventure.text.Component
 import org.slf4j.Logger
+import java.net.URL
 import java.nio.file.Files
 import java.nio.file.Path
+import java.security.MessageDigest
 import kotlin.io.path.createDirectories
 import kotlin.io.path.exists
 
@@ -24,11 +28,14 @@ import kotlin.io.path.exists
 	authors = ["trainb0y"]
 )
 class PackServe @Inject constructor(
-	val server: ProxyServer,
-	val logger: Logger,
+	private val server: ProxyServer,
+	private val logger: Logger,
 	@DataDirectory val dataDir: Path
 ) {
-	lateinit var config: Config
+	private lateinit var config: Config
+
+	private lateinit var packHash: ByteArray
+	private lateinit var packURL: String
 
 	@Subscribe
 	fun onProxyInit(event: ProxyInitializeEvent) {
@@ -45,5 +52,27 @@ class PackServe @Inject constructor(
 			.addFileSource(configPath.toFile())
 			.build()
 			.loadConfigOrThrow<Config>()
+
+		/*
+		packURL =  config.repository + "/archive/refs/heads/" + config.branch + ".zip"
+		packHash = config.hash.toByteArray()
+		if (packHash.size != 20) {
+			logger.warn("Attempting to get SHA-1 hash of resource pack, as it wasn't specified!")
+			packHash = MessageDigest
+				.getInstance("SHA-1")
+				.digest(URL(packURL).openConnection().getInputStream().readAllBytes())
+		}*/
+	}
+
+	@Subscribe
+	fun onResourcePack(event: PlayerChatEvent) {
+		logger.warn("sending pack")
+		val pack = server.createResourcePackBuilder(packURL)
+			.setHash(packHash)
+			.setPrompt(Component.text("Download! Or perish!"))
+			.setShouldForce(true)
+			.build()
+
+		event.player.sendResourcePackOffer(pack)
 	}
 }
